cfc9b5d0d6bfa84e36c606bea174668d6a2ed7b8
AngelTravieso <angeltraviesoc@gmail.com>
2023-11-01 11:48:58 -0400

    Realizar el mapeo de la respuesta de MovieDB

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -1,3 +1,5 @@
+import 'package:cinemapedia/infrastructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/moviedb_response.dart';
 import 'package:dio/dio.dart';
 
 import 'package:cinemapedia/config/constants/environment.dart';
@@ -18,8 +20,15 @@ class MoviedbDatasource extends MoviesDatasource {
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
     final response = await dio.get('/movie/now_playing');
+    final movieDBResponse = MovieDbResponse.fromJson(response.data);
 
-    final List<Movie> movies = [];
+    // Mapear resultados de la API a mi entidad Movie
+    final List<Movie> movies = movieDBResponse.results
+    // Evitar renderizar peliculas que no tienen posterPath
+    .where((moviedb) => moviedb.posterPath != 'no-poster')
+    .map(
+      (moviedb) => MovieMapper.movieDBToEntity(moviedb)
+      ).toList();
 
     return movies;
   }
--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -11,7 +11,7 @@ class MovieMapper {
         originalTitle: moviedb.originalTitle,
         overview: moviedb.overview,
         popularity: moviedb.popularity,
-        posterPath: (moviedb.posterPath != '') ? 'https://image.tmdb.org/t/p/w500/${moviedb.posterPath}' : 'https://ih1.redbubble.net/image.1893341687.8294/poster,504x498,f8f8f8-pad,600x600,f8f8f8.jpg',
+        posterPath: (moviedb.posterPath != '') ? 'https://image.tmdb.org/t/p/w500/${moviedb.posterPath}' : 'no-poster',
         releaseDate: moviedb.releaseDate,
         title: moviedb.title,
         video: moviedb.video,
